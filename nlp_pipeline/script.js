b449503891b9bb3c9e12828cfe84bccbb341f460
phanminhhoang-seo <hoangphan.180197@gmail.com>
2025-06-12 22:15:54 +0700

    Update script.js

--- nlp_pipeline/script.js
+++ nlp_pipeline/script.js
@@ -491,7 +491,7 @@ document.addEventListener('DOMContentLoaded', () => {
             resetPipeline();
             animationTimeoutId = setTimeout(() => { 
                 animatePipeline(); 
-            }, 20); 
+            }, 1000); 
         }
     };
 
